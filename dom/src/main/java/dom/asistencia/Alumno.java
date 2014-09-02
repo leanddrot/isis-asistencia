1476d169bc5725544106745eb864ceb324f566fe
leanddrot <leanddro@hotmail.com>
2014-09-02 00:31:05 -0300

    clase Alumno y AlumnoRepositorio

--- dom/src/main/java/dom/asistencia/Alumno.java
+++ dom/src/main/java/dom/asistencia/Alumno.java
@@ -1,5 +1,65 @@
 package dom.asistencia;
 
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.VersionStrategy;
+
+import org.apache.isis.applib.annotation.Bounded;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ObjectType;
+
+@javax.jdo.annotations.PersistenceCapable(identityType = IdentityType.DATASTORE)
+@javax.jdo.annotations.DatastoreIdentity(strategy = javax.jdo.annotations.IdGeneratorStrategy.IDENTITY, column = "id")
+@javax.jdo.annotations.Version(strategy = VersionStrategy.VERSION_NUMBER, column = "version")
+@ObjectType("alumno")
+@Bounded
 public class Alumno {
 
+	// {{ Nombre (property)
+	private String nombre;
+
+	@MemberOrder(sequence = "1")
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(final String nombre) {
+		this.nombre = nombre;
+	}
+	// }}
+
+	// {{ Apellido (property)
+	private String apellido;
+
+	@MemberOrder(sequence = "2")
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(final String apellido) {
+		this.apellido = apellido;
+	}
+	// }}
+
+	// {{ Dni (property)
+	private String dni;
+
+	@MemberOrder(sequence = "3")
+	@javax.jdo.annotations.Column(allowsNull = "false")
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(final String dni) {
+		this.dni = dni;
+	}
+	// }}
+
+	public String title(){
+		return this.getNombre() + " " + this.getApellido();
+	}
+
+	
+	
 }
---
+++ dom/src/main/java/dom/asistencia/AlumnoRepositorio.java
@@ -0,0 +1,91 @@
+package dom.asistencia;
+
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.ActionSemantics;
+import org.apache.isis.applib.annotation.Bookmarkable;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.Hidden;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Named;
+import org.apache.isis.applib.annotation.Where;
+import org.apache.isis.applib.annotation.ActionSemantics.Of;
+
+
+@DomainService(menuOrder = "25", repositoryFor = Alumno.class)
+@Named("Alumnos")
+public class AlumnoRepositorio {
+	
+	// region > identification in the UI
+		// //////////////////////////////////////
+
+		public String getId() {
+			return "alumno";
+		}
+
+		public String iconName() {
+			return "SimpleObject";
+		}
+
+		// endregion
+
+		// region > listAll (action)
+		// //////////////////////////////////////
+
+		@Bookmarkable
+		@ActionSemantics(Of.SAFE)
+		@MemberOrder(sequence = "1")
+		@Named("Listar Alumnos")
+		public List<Alumno> listAll() {
+			return container.allInstances(Alumno.class);
+		}
+
+		// endregion
+
+		// region > create (action)
+		// //////////////////////////////////////
+
+		@MemberOrder(sequence = "2")
+		@Named("Crear Alumno")
+		@Hidden(where = Where.OBJECT_FORMS)
+		public Alumno create(final @Named("Nombre") String nombre,
+				final @Named("Apellido") String apellido,
+				final @Named("Dni") String dni) {
+			final Alumno obj = container.newTransientInstance(Alumno.class);
+
+			obj.setNombre(nombre);
+			obj.setApellido(apellido);
+			obj.setDni(dni);
+			container.persistIfNotAlready(obj);
+			return obj;
+		}
+
+		// endregion
+
+		// region > remove User (action)
+		// //////////////////////////////////////
+
+		@ActionSemantics(Of.NON_IDEMPOTENT)
+		@MemberOrder(sequence = "4")
+		@Named("Borrar Alumno")
+		public String borrarAlumno(@Named("User") Alumno alumno) {
+			String nombre = alumno.title();
+			container.remove(alumno);
+			return "El alumno " + nombre + " ha sido eliminado";
+		}
+
+		// endregion
+
+
+		// region > injected services
+		// //////////////////////////////////////
+
+		@javax.inject.Inject
+		DomainObjectContainer container;
+
+		// endregion
+		
+}
